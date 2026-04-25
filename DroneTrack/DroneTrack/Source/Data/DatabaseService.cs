519503c41cc351ad2b3f6e146c7361cb5ff71192
Wiktoria Szczepańska <87717209+ArcherWike@users.noreply.github.com>
2026-04-25 17:41:35 +0200

    added database initialization

---
+++ DroneTrack/DroneTrack/Source/Data/DatabaseService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DroneTrack.Source.Models;
+using Microsoft.Data.Sqlite;
+
+namespace DroneTrack.Source.Data
+{
+    public class DatabaseService
+    {
+        DroneDatabaseContext database = new DroneDatabaseContext();
+
+        public DatabaseService()
+        {
+            database.Database.EnsureCreated();
+        }
+    }
+}
---
+++ DroneTrack/DroneTrack/Source/Data/DroneDatabaseContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.IO;
+
+using DroneTrack.Source.Models;
+
+namespace DroneTrack.Source.Data
+{
+    class DroneDatabaseContext : DbContext
+    { 
+        public DbSet<Drone> Drones { get; set; }
+        public DbSet<FlightLog> FlightLogs { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "drone_data.db");
+            optionsBuilder.UseSqlite($"Data Source={dbPath}");
+        }
+    }
+}
