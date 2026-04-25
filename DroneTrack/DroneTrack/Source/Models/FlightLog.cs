c3993cedc188312476d8b6b5063ea68f6f103ea9
Wiktoria Szczepańska <87717209+ArcherWike@users.noreply.github.com>
2026-04-25 15:12:34 +0200

    Created DataBase class (drone, flightLog)

---
+++ DroneTrack/DroneTrack/Source/Models/FlightLog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DroneTrack.Source.Models
+{
+    public class FlightLog
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int OperatorIdentity { get; set; }
+        [Required]
+        public int DroneId { get; set; }
+        [Required]
+        public DateTime FlightDateStart { get; set; } = DateTime.Now;
+        [Required]
+        public DateTime FlightDateEnd { get; set; }
+        [Required]
+        public double Latitude { get; set; }
+        [Required]
+        public double Longitude { get; set; }
+        [Required]
+        public double MaxAltitude { get; set; }
+        public string Description { get; set; }
+    }
+}
